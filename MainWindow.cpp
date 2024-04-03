716428b593d2aacd005edd69521b5f348be10ef5
ackimixs <ackimixs@protonmail.com>
2024-04-03 20:07:14 +0200

    change tcp message

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -56,6 +56,7 @@ MainWindow::MainWindow(const char *address, int port, QWidget *parent) : QMainWi
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         this->tcpClient->sendMessage("ihm;servo_pot;ouvrir pince;3");
     });
+
     connect(this->homologation, &Homologation::replierClicked, [&]() {
         this->tcpClient->sendMessage("ihm;servo_pot;lever bras;1");
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
@@ -90,7 +91,9 @@ MainWindow::MainWindow(const char *address, int port, QWidget *parent) : QMainWi
 
     this->testMode = new TestMode(centralWidget);
     connect(this->testMode, &TestMode::goPressed, [&](int x, int y, int theta) {
-        this->tcpClient->sendMessage("ihm;arduino;go;" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(theta));
+        this->tcpClient->sendMessage("ihm;arduino;go;" + std::to_string(x) + "," + std::to_string(y));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        this->tcpClient->sendMessage("ihm;arduino;angle;" + std::to_string(theta));
     });
 
     this->inGame = new InGame(teamChooser);
